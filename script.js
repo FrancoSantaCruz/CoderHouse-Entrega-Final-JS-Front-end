92b705f2a70f1c8e58510436dc83a86655210820
FrancoSantaCruz <sczfranco@gmail.com>
2023-03-21 22:46:34 -0300

    Arreglando github pages

--- script.js
+++ script.js
@@ -85,7 +85,7 @@ userForm.addEventListener("submit", (e) => {
     userForm.style.display = "none";
 
     let newCardImage = document.createElement("img");
-    newCardImage.setAttribute("src", "./img/empty-card.png");
+    newCardImage.setAttribute("src", "."+"/img/empty-card.png");
     newCardImage.setAttribute("alt", "Card Image");
     newCardImage.setAttribute("class", "img __card mrg__aux");
 
@@ -182,7 +182,7 @@ function operaciones(montoAgregarQuitar) {
             `
 
             let newExtractorDineroImg = document.createElement("img");
-            newExtractorDineroImg.setAttribute("src", "./img/extraccionDinero.png");
+            newExtractorDineroImg.setAttribute("src", "."+"/img/extraccionDinero.png");
             newExtractorDineroImg.setAttribute("alt", "Extractor de Dinero");
             newExtractorDineroImg.setAttribute("class", "img __extractorDinero");
 
